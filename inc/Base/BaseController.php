597b4475cb1926f108ff56f76286e59ab1887408
mohammedrezq <mohammedrezq2000@gmail.com>
2021-08-11 20:13:45 +0200

    Finish Some Plugin APIs

--- inc/Base/BaseController.php
+++ inc/Base/BaseController.php
@@ -21,12 +21,6 @@ class BaseController {
         $this->plugin_path = plugin_dir_path(dirname(__FILE__, 2));
         $this->plugin_url = plugin_dir_url(dirname(__FILE__, 2));
         $this->plugin = plugin_basename(dirname(__FILE__, 3)) . '/survey-kit.php';
-
-        // TODO: Remove this
-        echo '<pre>';
-        print_r($this->plugin);
-        echo '</pre>';
-        
     }
 
 
