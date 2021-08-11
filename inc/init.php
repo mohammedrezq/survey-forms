a248b1c525d6a6e8c81ddd566dae7a7c47f03be1
mohammedrezq <mohammedrezq2000@gmail.com>
2021-08-11 18:57:52 +0200

    Add Init Structure

--- inc/init.php
+++ inc/init.php
@@ -3,3 +3,37 @@
  * 
  * @package SurveyKit
  */
+
+namespace Inc;
+
+final class Init {
+
+    /**
+	 * Store all the classes inside an array
+	 *
+	 * @return array Full list of classes
+	 */
+    public static function get_services() {
+        return [];
+    }
+
+    public static function register_services() {
+        foreach(self::get_services() as $class) {
+            $service = self::instaniate($class);
+            if(method_exists($service, 'register')) {
+                $service->register();
+            }
+        }
+    }
+
+	/**
+	 * Initialize the class
+	 *
+	 * @param class $class class from the services array
+	 * @return class instance new instance of the class
+	 */
+    private static function instaniate($class) {
+        $service = new $class();
+        return $service;
+    }
+}
